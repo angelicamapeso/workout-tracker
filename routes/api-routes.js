450cad90341eeb164adf3a655dc866a8653831d2
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-12-05 00:12:34 -0500

    Modify error code to 500

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,7 +6,7 @@ router.get('/workouts', async function(req, res) {
     const workouts = await db.Workout.find({});
     res.status(200).json(workouts);
   } catch(err) {
-    res.status(404).json({ error: err });
+    res.status(500).json({ error: err });
   }
 });
 
