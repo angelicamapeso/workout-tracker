8deba95abd427352faa760e4a2f16d6b10724a27
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-12-05 22:27:34 -0500

    Format api routes file

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,35 +1,36 @@
 const router = require('express').Router();
 const db = require('../models');
 
-router.get('/workouts', async function(req, res) {
+router.get('/workouts', async function (req, res) {
   try {
     const workouts = await db.Workout.find({});
     res.status(200).json(workouts);
-  } catch(err) {
+  } catch (err) {
     res.status(500).json({ error: err });
   }
 });
 
-router.post('/workouts', async function(req, res) {
+router.post('/workouts', async function (req, res) {
   try {
+    // only create it if a workout doesn't exist for that day
     const newWorkout = await db.Workout.create(req.body);
     res.status(201).json(newWorkout);
-  } catch(err) {
+  } catch (err) {
     res.status(500).json({ error: err });
   }
 });
 
-router.put('/workouts/:id', async function(req, res) {
+router.put('/workouts/:id', async function (req, res) {
   const id = req.params.id;
 
   try {
-    const workout = await db.Workout.findOne({ _id: id});
+    const workout = await db.Workout.findOne({ _id: id });
     if (!workout) res.status(404).json({ error: 'Workout not found.' });
 
     workout.exercises.push(req.body);
     workout.save();
     res.status(200).json(workout);
-  } catch(err) {
+  } catch (err) {
     res.status(500).json({ error: err });
     console.log(err);
   }
